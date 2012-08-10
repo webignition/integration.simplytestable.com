675771c1b67709bcfb4726a4fb97656872870783
Jon Cram <jon@webignition.net>
2012-08-10 14:55:30 +0100

    test 01

--- src/SimplyTestable/Integration/Tests/IntegrationTest.php
+++ src/SimplyTestable/Integration/Tests/IntegrationTest.php
@@ -52,41 +52,43 @@ class IntegrationTest extends \PHPUnit_Framework_TestCase {
   
 
     public function testPrepareEnvironment() {
+        var_dump(getenv('SIMPLYTESTABLE_INTEGRATION_PREPARE'));
+        
         if (getenv('SIMPLYTESTABLE_INTEGRATION_PREPARE')) {
             $this->resetEnvironmentDatabases();
-            $this->requestWorkerActivation();
-            $this->verifyWorkerActivation();            
+            //$this->requestWorkerActivation();
+            //$this->verifyWorkerActivation();            
         }
     }    
 
-    /**
-     *
-     * @depends testPrepareEnvironment 
-     */
-    public function testNewJobRequest() { 
-        $request = $this->getAuthorisedHttpRequest('http://ci.app.simplytestable.com/tests/http://webignition.net/start/');        
-        $response = $this->getHttpClient()->getResponse($request);
-        
-        $responseObject = json_decode($response->getBody());
-        
-        $this->assertEquals(self::HTTP_STATUS_OK, $response->getResponseCode());
-        $this->assertEquals(self::PUBLIC_USER_USERNAME, $responseObject->user);
-        $this->assertEquals('http://webignition.net/', $responseObject->website);
-        $this->assertEquals('new', $responseObject->state);
-        $this->assertEquals(0, count($responseObject->tasks));
-        
-        $this->jobId = $responseObject->id;
-    }
-    
-    
-    /**
-     * @depends testNewJobRequest
-     */
-    public function testPrepareNewJob() {
-        if (getenv('SIMPLYTESTABLE_INTEGRATION_PREPARE')) {
-            $this->runSymfonyCommand($this->coreApplication, 'simplytestable:job:prepare ' . $this->jobId);
-        }
-    }     
+//    /**
+//     *
+//     * @depends testPrepareEnvironment 
+//     */
+//    public function testNewJobRequest() { 
+//        $request = $this->getAuthorisedHttpRequest('http://ci.app.simplytestable.com/tests/http://webignition.net/start/');        
+//        $response = $this->getHttpClient()->getResponse($request);
+//        
+//        $responseObject = json_decode($response->getBody());
+//        
+//        $this->assertEquals(self::HTTP_STATUS_OK, $response->getResponseCode());
+//        $this->assertEquals(self::PUBLIC_USER_USERNAME, $responseObject->user);
+//        $this->assertEquals('http://webignition.net/', $responseObject->website);
+//        $this->assertEquals('new', $responseObject->state);
+//        $this->assertEquals(0, count($responseObject->tasks));
+//        
+//        $this->jobId = $responseObject->id;
+//    }
+//    
+//    
+//    /**
+//     * @depends testNewJobRequest
+//     */
+//    public function testPrepareNewJob() {
+//        if (getenv('SIMPLYTESTABLE_INTEGRATION_PREPARE')) {
+//            $this->runSymfonyCommand($this->coreApplication, 'simplytestable:job:prepare ' . $this->jobId);
+//        }
+//    }     
     
     private function resetEnvironmentDatabases() {
         foreach ($this->environments as $environment => $path) {
