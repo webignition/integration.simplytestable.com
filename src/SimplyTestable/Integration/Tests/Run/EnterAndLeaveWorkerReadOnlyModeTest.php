a16ef2d8ad574c34227098ad0e205bf0776f4b12
Jon Cram <jon@webignition.net>
2013-02-25 14:11:56 +0000

    Inc

--- src/SimplyTestable/Integration/Tests/Run/EnterAndLeaveWorkerReadOnlyModeTest.php
+++ src/SimplyTestable/Integration/Tests/Run/EnterAndLeaveWorkerReadOnlyModeTest.php
@@ -30,10 +30,46 @@ class EnterAndLeaveWorkerReadOnlyModeTest extends BaseTestSequenceTest {
         
         $preAssignmentTasks = $this->getTasks();
         $tasksToAssign = array_slice($preAssignmentTasks, 0, 10);
-        var_dump(count($preAssignmentTasks), count($tasksToAssign));
-////        foreach ($preAssignmentTasks as $task) {
-////            $this->runSymfonyCommand(self::$coreApplication, 'simplytestable:task:assign ' . $task->id);
-////        }        
+
+        foreach ($tasksToAssign as $task) {
+            $this->runSymfonyCommand(self::$coreApplication, 'simplytestable:task:assign ' . $task->id);
+        }
+        
+        // Verify job state
+        $job = $this->getJob();
+        $this->assertEquals(self::HTTP_STATUS_OK, self::$lastHttpResponse->getResponseCode());
+        $this->assertEquals(self::$jobId, $job->id);
+        $this->assertEquals(self::PUBLIC_USER_USERNAME, $job->user);
+        $this->assertEquals(self::TEST_CANONICAL_URL, $job->website);
+        $this->assertEquals('in-progress', $job->state);
+        $this->assertGreaterThan(0, $job->url_count);
+        $this->assertGreaterThan(0, $job->task_count);       
+        $this->assertNotNull($job->time_period);
+        $this->assertNotNull($job->time_period->start_date_time);
+        $this->assertTrue(!isset($job->time_period->end_date_time));
+        
+        
+        // Verify state of all tasks
+        $postAssignmentTasks = $this->getTasks();       
+        
+        $this->assertEquals(self::HTTP_STATUS_OK, self::$lastHttpResponse->getResponseCode());
+
+        foreach ($postAssignmentTasks as $task) {
+            $this->assertGreaterThan(0, $task->id);
+            $this->assertNotNull($task->url);
+            
+            if ($task->id <= 10) {
+                $this->assertEquals('in-progress', $task->state);
+            } else {
+                $this->assertEquals('queued', $task->state);
+            }            
+            
+            $this->assertNotEquals('', $task->worker);
+            $this->assertNotEquals('', $task->type);
+            $this->assertNotNull($task->time_period);
+            $this->assertNotNull($task->time_period->start_date_time);
+            $this->assertTrue(!isset($task->time_period->end_date_time));
+        }        
         
     }
     
