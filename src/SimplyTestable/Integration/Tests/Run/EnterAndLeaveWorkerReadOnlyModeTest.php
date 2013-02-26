e032e35c9c59e0efb52bed19ae3840a66e838e0a
Jon Cram <jon@webignition.net>
2013-02-26 10:52:40 +0000

    Completed EnterAndLeaveWorkerReadOnlyModeTest

--- src/SimplyTestable/Integration/Tests/Run/EnterAndLeaveWorkerReadOnlyModeTest.php
+++ src/SimplyTestable/Integration/Tests/Run/EnterAndLeaveWorkerReadOnlyModeTest.php
@@ -167,8 +167,8 @@ class EnterAndLeaveWorkerReadOnlyModeTest extends BaseTestSequenceTest {
      * @depends testWorkerEnterReadOnlyAfterPerformingTasks
      */
     public function testReportTaskCompletionWhenWorkersAreReadOnly() {
-        $prePerformTasks = $this->getTasks();
-        foreach ($prePerformTasks as $task) {
+        $tasks = $this->getTasks();
+        foreach ($tasks as $task) {
             if ($task->id <= 10) {
                 $result = $this->runSymfonyCommand($task->worker, 'simplytestable:task:reportcompletion ' . $task->remote_id);
                 $this->assertEquals('Unable to report completion, worker application is in maintenance read-only mode', trim($result));
@@ -206,101 +206,20 @@ class EnterAndLeaveWorkerReadOnlyModeTest extends BaseTestSequenceTest {
             $workerStatus = json_decode($statusResponse->getBody());
             $this->assertEquals('active', $workerStatus->state);
         }
-    }    
+    }
     
     
-    
-//    /**
-//     * @depends testPrepare
-//     */
-//    public function testAssignTasksToWorkers() {
-//        // Assign each task out to the workers
-//        $preAssignmentTasks = $this->getTasks();
-//        foreach ($preAssignmentTasks as $task) {
-//            $this->runSymfonyCommand(self::$coreApplication, 'simplytestable:task:assign ' . $task->id);
-//        }
-//        
-//        // Verify job state
-//        $job = $this->getJob();
-//        $this->assertEquals(self::HTTP_STATUS_OK, self::$lastHttpResponse->getResponseCode());
-//        $this->assertEquals(self::$jobId, $job->id);
-//        $this->assertEquals(self::PUBLIC_USER_USERNAME, $job->user);
-//        $this->assertEquals(self::TEST_CANONICAL_URL, $job->website);
-//        $this->assertEquals('in-progress', $job->state);
-//        $this->assertGreaterThan(0, $job->url_count);
-//        $this->assertGreaterThan(0, $job->task_count);       
-//        $this->assertNotNull($job->time_period);
-//        $this->assertNotNull($job->time_period->start_date_time);
-//        $this->assertTrue(!isset($job->time_period->end_date_time));
-//        
-//        
-//        // Verify state of all tasks
-//        $postAssignmentTasks = $this->getTasks();       
-//        
-//        $this->assertEquals(self::HTTP_STATUS_OK, self::$lastHttpResponse->getResponseCode());
-//
-//        foreach ($postAssignmentTasks as $task) {
-//            $this->assertGreaterThan(0, $task->id);
-//            $this->assertNotNull($task->url);
-//            $this->assertEquals('in-progress', $task->state);
-//            $this->assertNotEquals('', $task->worker);
-//            $this->assertNotEquals('', $task->type);
-//            $this->assertNotNull($task->time_period);
-//            $this->assertNotNull($task->time_period->start_date_time);
-//            $this->assertTrue(!isset($task->time_period->end_date_time));
-//        }        
-//    }
-//    
-//    
-//    /**
-//     * @depends testAssignTasksToWorkers
-//     */
-//    public function testPerformTasks() { 
-//        $prePerformTasks = $this->getTasks();
-//        foreach ($prePerformTasks as $task) {
-//            $this->runSymfonyCommand($task->worker, 'simplytestable:task:perform ' . $task->remote_id);
-//        }           
-//    }
-//    
-//    
-//    /**
-//     * @depends testPerformTasks
-//     */
-//    public function testReportTaskCompletion() {
-//        // Report completion for all tasks
-//        $preReportCompletionTasks = $this->getTasks();        
-//        foreach ($preReportCompletionTasks as $task) {
-//            $this->runSymfonyCommand($task->worker, 'simplytestable:task:reportcompletion ' . $task->remote_id);
-//        }     
-//        
-//        // Verify job state
-//        $job = $this->getJob();        
-//        $this->assertEquals(self::HTTP_STATUS_OK, self::$lastHttpResponse->getResponseCode());
-//        $this->assertEquals(self::$jobId, $job->id);
-//        $this->assertEquals(self::PUBLIC_USER_USERNAME, $job->user);
-//        $this->assertEquals(self::TEST_CANONICAL_URL, $job->website);
-//        $this->assertEquals('completed', $job->state);
-//        $this->assertGreaterThan(0, $job->url_count);
-//        $this->assertGreaterThan(0, $job->task_count);       
-//        $this->assertNotNull($job->time_period);
-//        $this->assertNotNull($job->time_period->start_date_time);
-//        $this->assertNotNull($job->time_period->end_date_time);
-//        
-//        // Verify state of all tasks
-//        $tasks = $this->getTasks();    
-//
-//        $this->assertEquals(self::HTTP_STATUS_OK, self::$lastHttpResponse->getResponseCode());
-//
-//        foreach ($tasks as $task) {
-//            $this->assertGreaterThan(0, $task->id);
-//            $this->assertNotNull($task->url);
-//            $this->assertEquals('completed', $task->state);
-//            $this->assertEquals('', $task->worker);
-//            $this->assertNotEquals('', $task->type);
-//            $this->assertNotNull($task->time_period);
-//            $this->assertNotNull($task->time_period->start_date_time);
-//            $this->assertNotNull($task->time_period->end_date_time);
-//        }         
-//    }
+    /**
+     * @depends testLeaveReadOnlyModeAfterReportingTaskCompletion
+     */
+    public function testReportCompletionAfterLeavingReadOnlyMode() {
+        $tasks = $this->getTasks();
+        foreach ($tasks as $task) {
+            if ($task->id <= 10) {
+                $result = $this->runSymfonyCommand($task->worker, 'simplytestable:task:reportcompletion ' . $task->remote_id);
+                $this->assertEquals('Reported task completion ['.$task->remote_id.']', trim($result));
+            }
+        }          
+    }
       
 }
