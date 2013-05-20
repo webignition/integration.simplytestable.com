ce3c572c96faa5e7f3972c35632dec8568c8d99c
Jon Cram <jon@webignition.net>
2013-05-20 15:21:51 +0100

    Updated following single-URL instant prep

--- src/SimplyTestable/Integration/Tests/Run/BaseTestSequenceTest.php
+++ src/SimplyTestable/Integration/Tests/Run/BaseTestSequenceTest.php
@@ -75,7 +75,7 @@ abstract class BaseTestSequenceTest extends BaseTest {
         $this->assertInternalType('integer', $responseObject->id);
         $this->assertEquals(self::PUBLIC_USER_USERNAME, $responseObject->user);
         $this->assertEquals(self::TEST_CANONICAL_URL, $responseObject->website);
-        $this->assertEquals('new', $responseObject->state);
+        $this->assertEquals('queued', $responseObject->state);
         $this->assertEquals(0, $responseObject->url_count);
         $this->assertEquals(0, $responseObject->task_count);
         
